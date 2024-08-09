9681643dcd4a0d528d750b9214c15a3e8188db46
Antonio Pires <tonan@outlook.com.br>
2024-08-09 07:47:20 -0300

    Installing the graphics library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import CompanyMetrics from "./app/features/CompanyMetrics";
+import {Col, Row, Typography} from "antd";
+
+const { Title, Paragraph } = Typography;
+
 function App() {
-    return <div>app</div>;
+    return (
+        <Row>
+            <Col span={24}>
+                <Title level={2}>Olá, José Sousa</Title>
+                <Paragraph>
+                    Esse é o resumo da empresa nos últimos doze meses
+                </Paragraph>
+            </Col>
+            <Col span={24}>
+                <CompanyMetrics/>
+            </Col>
+        </Row>
+    );
 }
 
 export default App;
