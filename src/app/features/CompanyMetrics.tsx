51b9f2314d3960c3273a41fc54b8272c09175f18
Antonio Pires <tonan@outlook.com.br>
2024-08-11 10:52:33 -0300

    Disabling the vertical axis on the graph

--- src/app/features/CompanyMetrics.tsx
+++ src/app/features/CompanyMetrics.tsx
@@ -53,6 +53,7 @@ export default function CompanyMetrics() {
                 }
             }
         },
+        yAxis: false,
         xAxis: {
             label: {
                 formatter(item) {
