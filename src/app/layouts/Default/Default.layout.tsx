86760c1ff7b38c78e6f603b36aa84baf7b23135f
Antonio Pires <tonan@outlook.com.br>
2024-08-12 07:18:50 -0300

    Preventing "shrink" in page content

--- src/app/layouts/Default/Default.layout.tsx
+++ src/app/layouts/Default/Default.layout.tsx
@@ -13,7 +13,7 @@ export default function DefaultLayout(props: DefaultLayoutProps) {
     return (
         <Layout>
             <DefaultLayoutHeader />
-            <Layout>
+            <Layout id={'PageLayout'}>
                 <DefaultLayoutSidebar />
                 <Layout style={{padding: '0 24px 24px'}}>
                     <Breadcrumb />
