d23ed925daf0ea09366f3a010bcae536a1dd457e
Antonio Pires <tonan@outlook.com.br>
2024-08-22 06:34:32 -0300

    Validating with enum

--- src/app/features/UserForm.tsx
+++ src/app/features/UserForm.tsx
@@ -211,6 +211,11 @@ export default function UserForm() {
                                 required: true,
                                 message: 'O campo é obrigatório',
                             },
+                            {
+                                type: 'enum',
+                                enum: ['EDITOR', 'ASSISTANT', 'MANAGER'],
+                                message: 'O perfil precisa ser editor, assistente ou gerente',
+                            }
                         ]}
                     >
                         <Select placeholder={'Selecione um perfil'}>
