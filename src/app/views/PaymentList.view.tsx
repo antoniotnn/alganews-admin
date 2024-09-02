a0098d4fb6edcf7aa9063d4b31ff1b79c0180998
Antonio Pires <tonan@outlook.com.br>
2024-09-02 07:04:08 -0300

    Payments Table

--- src/app/views/PaymentList.view.tsx
+++ src/app/views/PaymentList.view.tsx
@@ -1,3 +1,71 @@
+import {Table, Tag} from "antd";
+import {Payment} from "tnn-sdk";
+import usePayments from "../../core/hooks/usePayments";
+import {useEffect} from "react";
+import moment from "moment";
+
 export default function PaymentListView() {
-    return <div>todo: payment list view</div>;
+    const {payments, fetchPayments } = usePayments();
+
+    useEffect(() => {
+        fetchPayments({
+            sort: ['scheduledTo', 'desc'],
+            page: 0
+        });
+    }, [fetchPayments]);
+
+    return <>
+        <Table <Payment.Summary>
+            dataSource={payments?.content}
+            columns={[
+                {
+                    dataIndex: 'id',
+                    title: '#'
+                },
+                {
+                    dataIndex: 'payee',
+                    title: 'Editor',
+                    render(payee: Payment.Summary['payee']) {
+                        return payee.name;
+                    }
+                },
+                {
+                    dataIndex: 'scheduledTo',
+                    title: 'Agendamento',
+                    align: 'center',
+                    render(date: string) {
+                        return moment(date).format('DD/MM/YYYY');
+                    }
+                },
+                {
+                    dataIndex: 'accountingPeriod',
+                    title: 'Período',
+                    align: 'center',
+                    render(period: Payment.Summary['accountingPeriod']) {
+                        const starts = moment(period.startsOn).format('DD/MM/YYYY');
+                        const ends = moment(period.endsOn).format('DD/MM/YYYY');
+                        return `${starts} - ${ends}`;
+                    }
+                },
+                {
+                    dataIndex: 'approvedAt',
+                    title: 'Status',
+                    align: 'center',
+                    render(approvalDate: Payment.Summary['approvedAt']) {
+                        const formattedApprovalDate = moment(approvalDate).format('DD/MM/YYYY');
+
+                        return (
+                            <Tag color={approvalDate ? 'green' : 'warning'}>
+                                {
+                                    approvalDate ?
+                                        `Aprovado em ${formattedApprovalDate}`
+                                        : 'Aguardando aprovação'
+                                }
+                            </Tag>
+                        );
+                    }
+                },
+            ]}
+        />
+    </>;
 }
---
+++ src/core/hooks/usePayments.ts
@@ -0,0 +1,19 @@
+import {Payment, PaymentService} from "tnn-sdk";
+import {useCallback, useState} from "react";
+
+export default function usePayments() {
+    const [payments, setPayments] = useState<Payment.Paginated>();
+
+    const fetchPayments = useCallback(
+        async (query: Payment.Query) => {
+            const payments = await PaymentService.getAllPayments(query);
+            setPayments(payments);
+        },
+        []
+    );
+
+    return {
+        payments,
+        fetchPayments
+    }
+}
