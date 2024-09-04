de2bb717bc973e886ff6f7c7a417230441115c60
Antonio Pires <tonan@outlook.com.br>
2024-09-04 07:38:21 -0300

    Responsiveness at PaymentPosts

--- src/app/features/PaymentPosts.tsx
+++ src/app/features/PaymentPosts.tsx
@@ -1,4 +1,4 @@
-import { Table, Tooltip } from 'antd';
+import {Descriptions, Table, Tooltip} from 'antd';
 import { Payment, Post } from 'tnn-sdk';
 
 interface PaymentPostsProps {
@@ -11,12 +11,42 @@ export default function PaymentPosts(props: PaymentPostsProps) {
             <Table<Post.WithEarnings>
                 dataSource={props.posts}
                 pagination={false}
+                rowKey={'id'}
                 columns={[
+                    {
+                        responsive: ['xs'],
+                        title: 'Posts',
+                        render(post: Post.WithEarnings) {
+                            return <Descriptions column={1}>
+                                <Descriptions.Item label={'Título'}>
+                                    {post.title}
+                                </Descriptions.Item>
+                                <Descriptions.Item label={'Preço por palavra'}>
+                                    {post.earnings.pricePerWord.toLocaleString('pt-br', {
+                                        style: 'currency',
+                                        currency: 'BRL',
+                                        maximumFractionDigits: 2,
+                                    })}
+                                </Descriptions.Item>
+                                <Descriptions.Item label={'Palavras no post'}>
+                                    {post.earnings.words}
+                                </Descriptions.Item>
+                                <Descriptions.Item label={'Ganho no post'}>
+                                    {post.earnings.totalAmount.toLocaleString('pt-br', {
+                                        style: 'currency',
+                                        currency: 'BRL',
+                                        maximumFractionDigits: 2,
+                                    })}
+                                </Descriptions.Item>
+                            </Descriptions>
+                        }
+                    },
                     {
                         dataIndex: 'title',
                         title: 'Post',
                         ellipsis: true,
                         width: 300,
+                        responsive: ['sm'],
                         render(value: string) {
                             return <Tooltip title={value}>{value}</Tooltip>;
                         },
@@ -26,6 +56,7 @@ export default function PaymentPosts(props: PaymentPostsProps) {
                         title: 'Preço por palavra',
                         align: 'right',
                         width: 150,
+                        responsive: ['sm'],
                         render(price: number) {
                             return price.toLocaleString('pt-br', {
                                 style: 'currency',
@@ -39,12 +70,14 @@ export default function PaymentPosts(props: PaymentPostsProps) {
                         title: 'Palavras no post',
                         width: 150,
                         align: 'right',
+                        responsive: ['sm']
                     },
                     {
                         dataIndex: 'earnings.totalAmount'.split('.'),
                         title: 'Total ganho neste post',
                         align: 'right',
                         width: 170,
+                        responsive: ['sm']
                     },
                 ]}
             />
