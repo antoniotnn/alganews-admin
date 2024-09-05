dba7e04269238144e9c22fb1c8e6145cea3630ff
Antonio Pires <tonan@outlook.com.br>
2024-09-05 06:15:19 -0300

    search with payment parameter

--- src/app/views/PaymentDetails.view.tsx
+++ src/app/views/PaymentDetails.view.tsx
@@ -1,25 +1,46 @@
 import { Card, Divider } from 'antd';
 import moment from 'moment';
 import { useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import usePayment from '../../core/hooks/usePayment';
+import NotFoundError from '../components/NotFoundError';
 import PaymentBonuses from '../features/PaymentBonuses';
 import PaymentHeader from '../features/PaymentHeader';
 import PaymentPosts from '../features/PaymentPosts';
 
 export default function PaymentDetailsView() {
+    const params = useParams<{ id: string }>();
+    const history = useHistory();
+
     const {
         fetchPayment,
         fetchPosts,
         fetchingPayment,
         fetchingPosts,
+        postsNotFound,
+        paymentNotFound,
         payment,
         posts,
     } = usePayment();
 
     useEffect(() => {
-        fetchPosts(2);
-        fetchPayment(2);
-    }, [fetchPosts, fetchPayment]);
+        const paymentId = Number(params.id);
+        if (isNaN(paymentId)) {
+            return history.push('/pagamentos');
+        } else {
+            fetchPosts(paymentId);
+            fetchPayment(paymentId);
+        }
+    }, [fetchPosts, fetchPayment, params.id, history]);
+
+    if (paymentNotFound)
+        return (
+            <NotFoundError
+                title='Pagamento não encontrado'
+                actionDestination='/pagamentos'
+                actionTitle={'Ir para a lista de pagamentos'}
+            />
+        );
 
     return (
         <>
@@ -38,15 +59,9 @@ export default function PaymentDetailsView() {
                     totalEarnings={payment?.grandTotalAmount}
                 />
                 <Divider />
-                <PaymentBonuses
-                    loading={fetchingPayment}
-                    bonuses={payment?.bonuses}
-                />
+                <PaymentBonuses loading={fetchingPayment} bonuses={payment?.bonuses} />
                 <Divider />
-                <PaymentPosts
-                    loading={fetchingPosts}
-                    posts={posts}
-                />
+                <PaymentPosts loading={fetchingPosts} posts={posts} />
             </Card>
         </>
     );
--- src/core/hooks/usePayment.ts
+++ src/core/hooks/usePayment.ts
@@ -1,6 +1,7 @@
 import { Payment, PaymentService, Post } from 'tnn-sdk';
 import { useCallback } from 'react';
 import { useState } from 'react';
+import {ResourceNotFoundError} from "tnn-sdk/dist/errors";
 
 export default function usePayment() {
     const [posts, setPosts] = useState<Post.WithEarnings[]>([]);
@@ -9,11 +10,19 @@ export default function usePayment() {
     const [fetchingPosts, setFetchingPosts] = useState(false);
     const [fetchingPayment, setFetchingPayment] = useState(false);
 
+    const [paymentNotFound, setPaymentNotFound] = useState(false);
+    const [postsNotFound, setPostsNotFound] = useState(false);
+
     const fetchPayment = useCallback(async (paymentId: number) => {
         try {
             setFetchingPayment(true);
             const payment = await PaymentService.getExistingPayment(paymentId);
             setPayment(payment);
+        } catch (error) {
+            if (error instanceof ResourceNotFoundError) {
+                setPaymentNotFound(true);
+            }
+            throw error;
         } finally {
             setFetchingPayment(false);
         }
@@ -24,6 +33,11 @@ export default function usePayment() {
             setFetchingPosts(true);
             const posts = await PaymentService.getExistingPaymentPosts(paymentId);
             setPosts(posts);
+        } catch (error) {
+            if (error instanceof ResourceNotFoundError) {
+                setPostsNotFound(true);
+            }
+            throw error;
         } finally {
             setFetchingPosts(false);
         }
@@ -34,6 +48,8 @@ export default function usePayment() {
         fetchPosts,
         fetchingPayment,
         fetchingPosts,
+        paymentNotFound,
+        postsNotFound,
         posts,
         payment,
     };
