fb56c4522b1d88e26dd4c0fb406c9b49dab81453
Antonio Pires <tonan@outlook.com.br>
2024-09-11 16:35:28 -0300

    Basis of category management

--- src/app/features/EntryCategoryManager.tsx
+++ src/app/features/EntryCategoryManager.tsx
@@ -1,7 +1,34 @@
+import {Button, Row, Table} from "antd";
+import {CashFlow} from "tnn-sdk";
+
 export default function EntryCategoryManager() {
     return (
-        <div>
-            todo: implement entry category manager
-        </div>
+        <>
+            <Row justify={'space-between'}>
+                <Button>
+                    Atualizar categorias
+                </Button>
+                <Button>
+                    Adicionar categorias
+                </Button>
+            </Row>
+            <Table<CashFlow.CategorySummary>
+                dataSource={[]}
+                columns={[
+                    {
+                        title: 'Categoria',
+                        dataIndex: 'name',
+                        key: 'name',
+                    },
+                    {
+                        title: 'Total',
+                        dataIndex: 'total',
+                        key: 'total',
+                    },
+                ]}
+            >
+
+            </Table>
+        </>
     );
 }
