87bd3b2375e4178d60e246720ddad06b3e72b6f3
Antonio Pires <tonan@outlook.com.br>
2024-09-12 09:38:29 -0300

    Structuring the expense form

--- src/app/features/EntryForm.tsx
+++ src/app/features/EntryForm.tsx
@@ -1,7 +1,45 @@
+import {Button, Col, DatePicker, Divider, Form, Input, Row, Space} from "antd";
+import CurrencyInput from "../components/CurrencyInput";
+
 export default function EntryForm() {
     return (
-        <div>
-            todo: implement entry form
-        </div>
+        <Form layout={'vertical'}>
+            <Row gutter={16}>
+                <Col xs={24}>
+                    <Form.Item label={'Descrição'}>
+                        <Input placeholder={'Pagamento da AWS'} />
+                    </Form.Item>
+                </Col>
+                <Col xs={24}>
+                    <Form.Item label={'Categoria'}>
+                        <Input placeholder={'Pagamento da AWS'} />
+                    </Form.Item>
+                </Col>
+                <Col xs={24} lg={12}>
+                    <Form.Item label={'Montante'}>
+                        <CurrencyInput
+                            onChange={() => null}
+                        />
+                    </Form.Item>
+                </Col>
+                <Col xs={24} lg={12}>
+                    <Form.Item label={'Data de entrada'}>
+                        <DatePicker
+                            style={{ width: '100%' }}
+                            format={'DD/MM/YYYY'}
+                        />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Divider style={{marginTop: 0}}/>
+            <Row justify={'end'}>
+                <Space>
+                    <Button>Cancelar</Button>
+                    <Button type={'primary'} htmlType={'submit'}>
+                        Cadastrar despesa
+                    </Button>
+                </Space>
+            </Row>
+        </Form>
     );
 }
--- src/app/views/CashFlowExpenses.view.tsx
+++ src/app/views/CashFlowExpenses.view.tsx
@@ -1,5 +1,5 @@
 import { Button, Divider, Row, Space, Tooltip, Typography } from 'antd';
-import { InfoCircleFilled, TagOutlined } from '@ant-design/icons';
+import { InfoCircleFilled, TagOutlined, PlusCircleOutlined } from '@ant-design/icons';
 import EntriesList from '../features/EntriesList';
 import useCashFlow from '../../core/hooks/useCashFlow';
 import DoubleConfirm from '../components/DoubleConfirm';
@@ -7,15 +7,20 @@ import Modal from 'antd/lib/modal/Modal';
 import { useState } from 'react';
 import { useCallback } from 'react';
 import EntryCategoryManager from '../features/EntryCategoryManager';
+import EntryForm from "../features/EntryForm";
 const { Title, Text } = Typography;
 
 export default function CashFlowExpensesView() {
     const { selected, removeEntries } = useCashFlow('EXPENSE');
 
     const [showCategoryModal, setShowCategoryModal] = useState(false);
+    const [showFormModal, setShowFormModal] = useState(false);
 
     const openCategoryModal = useCallback(() => setShowCategoryModal(true), []);
     const closeCategoryModal = useCallback(() => setShowCategoryModal(false), []);
+
+    const openFormModal = useCallback(() => setShowFormModal(true), []);
+    const closeFormModal = useCallback(() => setShowFormModal(false), []);
 
     return (
         <>
@@ -28,6 +33,16 @@ export default function CashFlowExpensesView() {
                 destroyOnClose
             >
                 <EntryCategoryManager type={'EXPENSE'} />
+            </Modal>
+            <Modal
+                closeIcon={null}
+                visible={showFormModal}
+                onCancel={closeFormModal}
+                footer={null}
+                title={'Cadastrar despesa'}
+                destroyOnClose
+            >
+                <EntryForm  />
             </Modal>
             <Row justify={'space-between'} style={{ marginBottom: 16 }}>
                 <DoubleConfirm
@@ -48,13 +63,22 @@ export default function CashFlowExpensesView() {
                         Remover
                     </Button>
                 </DoubleConfirm>
-                <Button
-                    type={'primary'}
-                    icon={<TagOutlined />}
-                    onClick={openCategoryModal}
-                >
-                    Categorias
-                </Button>
+                <Space>
+                    <Button
+                        type={'primary'}
+                        icon={<TagOutlined />}
+                        onClick={openCategoryModal}
+                    >
+                        Categorias
+                    </Button>
+                    <Button
+                        type={'primary'}
+                        icon={<PlusCircleOutlined />}
+                        onClick={openFormModal}
+                    >
+                        Adicionar despesa
+                    </Button>
+                </Space>
             </Row>
             <Space direction={'vertical'}>
                 <Title level={3}>Recuperando entradas do mês de agosto</Title>
