c1651362f0baa2fde2aaa0c470f6e38f9e6db842
Antonio Pires <tonan@outlook.com.br>
2024-09-19 07:51:26 -0300

    Alternative UI for permission errors

--- src/app/features/CompanyMetrics.tsx
+++ src/app/features/CompanyMetrics.tsx
@@ -1,10 +1,12 @@
 import { Area, AreaConfig } from '@ant-design/charts';
 import { MetricService } from 'tnn-sdk';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
 import {format, parseISO} from 'date-fns';
 import transformDataIntoAntdChart from '../../core/utils/transformDataIntoAntdChart';
 import {ptBR} from "date-fns/locale";
+import {ForbiddenError} from "tnn-sdk/dist/errors";
+import {Card, Space, Typography} from "antd";
+import {LockFilled} from "@ant-design/icons";
 
 export default function CompanyMetrics() {
     const [data, setData] = useState<
@@ -15,11 +17,38 @@ export default function CompanyMetrics() {
         }[]
     >([]);
 
+    const [forbidden, setForbidden] = useState(false);
+
     useEffect(() => {
         MetricService.getMonthlyRevenuesExpenses()
             .then(transformDataIntoAntdChart)
-            .then(setData);
+            .then(setData)
+            .catch(err => {
+                if (err instanceof ForbiddenError) {
+                    setForbidden(true);
+                    return;
+                }
+                throw err;
+            })
     }, []);
+
+    if (forbidden) {
+        return (
+            <Card style={{ minHeight: 256, display: 'flex', alignItems: 'center' }}>
+                <Space direction={'vertical'}>
+                    <Space align={'center'}>
+                        <LockFilled style={{fontSize: 32}} />
+                        <Typography.Title style={{margin: 0}}>
+                            Acesso negado
+                        </Typography.Title>
+                    </Space>
+                    <Typography.Paragraph>
+                        Você não tem permissão para visualizar estes dados.
+                    </Typography.Paragraph>
+                </Space>
+            </Card>
+        );
+    }
 
     const config: AreaConfig = {
         data,
