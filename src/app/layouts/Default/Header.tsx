1fb2b0f21a6318354ae88bf46b744c5e17513fed
Antonio Pires <tonan@outlook.com.br>
2024-09-23 07:36:25 -0300

    Programmatic Logout + View Profile

--- src/app/layouts/Default/Header.tsx
+++ src/app/layouts/Default/Header.tsx
@@ -1,34 +1,39 @@
-import {Avatar, Card, Dropdown, Layout, Menu, Row, Tag} from "antd";
-import React from "react";
+import { Card, Dropdown, Layout, Menu, Row, Tag } from 'antd';
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
+import Avatar from 'antd/lib/avatar/avatar';
+import Meta from 'antd/lib/card/Meta';
 import logo from '../../../assets/logo.svg';
-import useAuth from "../../../core/hooks/useAuth";
-import Meta from "antd/es/card/Meta";
-import {UserOutlined} from "@ant-design/icons";
-import {LogoutOutlined} from "@ant-design/icons";
+import useAuth from '../../../core/hooks/useAuth';
+import { Link } from 'react-router-dom';
+import confirm from 'antd/lib/modal/confirm';
+import AuthService from '../../../auth/Authorization.service';
 
-const {Header} = Layout;
+const { Header } = Layout;
 
 export default function DefaultLayoutHeader() {
     const { user } = useAuth();
+
     return (
         <Header className='header no-print'>
             <Row
                 justify='space-between'
-                style={{ height: '100%', maxWidth: 1190, margin: '0 auto' }}
+                style={{
+                    height: '100%',
+                    maxWidth: 1190,
+                    margin: '0 auto',
+                }}
                 align='middle'
             >
-                <img src={logo} alt={'AlgaNews Admin'}/>
+                <img src={logo} alt={'AlgaNews Admin'} />
                 <Dropdown
                     placement={'bottomRight'}
-                    overlay={<>
-                        <Card style={{ width: 220 }}>
+                    overlay={
+                        <Menu style={{ width: 220 }}>
                             <Card bordered={false}>
                                 <Meta
                                     title={user?.name}
                                     description={
-                                        <Tag color={
-                                            user?.role === 'MANAGER' ? 'red' : 'blue'
-                                        }>
+                                        <Tag color={user?.role === 'MANAGER' ? 'red' : 'blue'}>
                                             {user?.role === 'EDITOR'
                                                 ? 'Editor'
                                                 : user?.role === 'MANAGER'
@@ -36,17 +41,35 @@ export default function DefaultLayoutHeader() {
                                                     : 'Assistente'}
                                         </Tag>
                                     }
-                                >
-                                </Meta>
+                                />
                             </Card>
-                            <Menu>
-                                <Menu.Item icon={<UserOutlined />}>Meu Perfil</Menu.Item>
-                                <Menu.Item icon={<LogoutOutlined />} danger>Fazer logout</Menu.Item>
-                            </Menu>
-                        </Card>
-                    </>}
+                            <Menu.Item icon={<UserOutlined />}>
+                                <Link to={`/usuarios/${user?.id}`}>Meu perfil</Link>
+                            </Menu.Item>
+                            <Menu.Item
+                                icon={<LogoutOutlined />}
+                                onClick={() =>
+                                    confirm({
+                                        title: 'Fazer logout',
+                                        content:
+                                            'Deseja realmente fazer o logout? Será necessário inserir as credenciais novamente.',
+                                        onOk() {
+                                            AuthService.imperativelySendToLogout();
+                                        },
+                                        closable: true,
+                                        okButtonProps: { danger: true },
+                                        okText: 'Fazer logout',
+                                        cancelText: 'Permanecer logado',
+                                    })
+                                }
+                                danger
+                            >
+                                Fazer logout
+                            </Menu.Item>
+                        </Menu>
+                    }
                 >
-                    <Avatar src={user?.avatarUrls.small}/>
+                    <Avatar src={user?.avatarUrls.small} />
                 </Dropdown>
             </Row>
         </Header>
